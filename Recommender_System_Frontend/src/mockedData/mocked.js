16bd825065171f40087bddc4d392f24ad0bf6c64
Ankit Kumar <44130267+Ankygurjar@users.noreply.github.com>
2023-11-19 14:40:08 -0500

    Update mocked.js

--- Recommender_System_Frontend/src/mockedData/mocked.js
+++ Recommender_System_Frontend/src/mockedData/mocked.js
@@ -314,6 +314,105 @@ export const randomRestaurants = [
         tag: "CCD0IJ02",
         rating: 3.7
     }
+];
+
+export const randomRestaurants2 = [
+    {
+        id: 1,
+        name: "Decardo",
+        tag: "Kl00002",
+        rating: 3.4
+    },
+    {
+        id: 2,
+        name: "JW Marriot",
+        tag: "Kl0D003",
+        rating: 5.0
+    },
+    {
+        id: 3,
+        name: "Rano Cafe",
+        tag: "PKO50023",
+        rating: 4.4
+    },
+    {
+        id: 4,
+        name: "Pizza Hut",
+        tag: "Kl00PL02",
+        rating: 4.5
+    },
+    {
+        id: 5,
+        name: "Dominoz",
+        tag: "CCD0IJ02",
+        rating: 3.7
+    }
+];
+
+export const randomRestaurants3 = [
+    {
+        id: 1,
+        name: "Hillside Cafe",
+        tag: "Kl00002",
+        rating: 3.4
+    },
+    {
+        id: 2,
+        name: "The Cafe @ JW",
+        tag: "Kl0D003",
+        rating: 5.0
+    },
+    {
+        id: 3,
+        name: "Rasoi",
+        tag: "PKO50023",
+        rating: 4.4
+    },
+    {
+        id: 4,
+        name: "The Hut",
+        tag: "Kl00PL02",
+        rating: 4.5
+    },
+    {
+        id: 5,
+        name: "Landour Restaurant",
+        tag: "CCD0IJ02",
+        rating: 3.7
+    }
+]
+
+export const randomRestaurants4 = [
+    {
+        id: 1,
+        name: "Swagat",
+        tag: "Kl00002",
+        rating: 3.4
+    },
+    {
+        id: 2,
+        name: "Baithak Restaurant",
+        tag: "Kl0D003",
+        rating: 5.0
+    },
+    {
+        id: 3,
+        name: "Barbeque Nation",
+        tag: "PKO50023",
+        rating: 4.4
+    },
+    {
+        id: 4,
+        name: "Pirates Of Grill",
+        tag: "Kl00PL02",
+        rating: 4.5
+    },
+    {
+        id: 5,
+        name: "Virgin Courtyard",
+        tag: "CCD0IJ02",
+        rating: 3.7
+    }
 ]
 
 export const mockedUsers = ["Karan", "Daksh", "Suraj", "Vivek"];
